0437c9eb6866a26983c307f70843340ca9b04282
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-11-16 11:50:30 +1300

    Webpack config and eslint

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: __dirname + '/client/prod',
+  entry: './index.jsx',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+        plugins: ['react-html-attrs', 'transform-decorators', 'transform-class-properties']
+      }
+    }]
+  },
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'budgetTripBundle.min.js'
+  }
+};
+
+
+
