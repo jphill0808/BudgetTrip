c058a59306aa22fb2ca51dbc4eafd44982774197
Jacob <jphill0808@gmail.com>
2017-11-16 18:50:04 -0500

    Complete Front-end Structure

---
+++ client/prod/components/Activities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Activities = (props) => {
+  return (
+    <div>
+      Hello Activities
+    </div>
+  );
+};
+
+export default Activities;
---
+++ client/prod/components/Budget.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Budget = (props) => {
+  return (
+    <div>
+      Hello Budget
+    </div>
+  );
+};
+
+export default Budget;
