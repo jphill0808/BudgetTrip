a38057744d529488b993c8ef8d39d7419d700fe9
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-11-17 16:15:18 +1300

    Commit before rebase

--- client/prod/components/search.jsx
+++ client/prod/components/search.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const axios = require('axios');
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      budget: null,
+      location: null,
+      startingDate: null,
+      endDate: null
+    };
+  }
+
+  onChange(event){
+    event.target.value
+  }
+
+  search(event) {
+    event.preventDefault();
+
+
   }
 
   render() {
     return (
-      <div className='container'>
-        <label htmlFor="budget">Budget</label>
-        <input type="text" name="budget" />
-        <label htmlFor="location">Location</label>
-        <input type="text" align="right" name="location" />
-        <label htmlFor=""></label>
-        <input type="date"/>
-        <label htmlFor=""></label>
-        <input type="date"/>
-        <button type="submit" value="Submit">Submit</button>
+      <div className="container">
+        <form onSubmit={this.search} action="/search" method="POST">
+          <label htmlFor="budget">Budget</label>
+          <input type="text" name="budget" onChange={this.onChange.bind(this)}/>
+          <label htmlFor="location">Location</label>
+          <input type="text" align="right" name="location" />
+          <label htmlFor=""></label>
+          <input type="date" name="start-date" />
+          <label htmlFor=""></label>
+          <input type="date" name="end-date" />
+          <button type="submit" value="Submit"> Submit </button>
+        </form>
       </div>
     );
   }
