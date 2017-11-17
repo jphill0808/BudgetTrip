cc346cc557c5a4e4848ab426232e97966b7e6c58
Jacob <jphill0808@gmail.com>
2017-11-17 13:23:57 -0500

    progress

--- client/prod/components/App.jsx
+++ client/prod/components/App.jsx
@@ -1,54 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './login.jsx';
-import { Router, Route, BrowserRouter, Redirect, withRouter } from 'react-router-dom';
 import Search from './Search.jsx';
-
-import Auth from '../../../Auth/Auth.js';
-const auth = new Auth();
-
-auth.handleAuthentication();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isLoggedIn: false
-    }
   }
 
 
-
-
   render() {
-    if (auth.isAuthenticated()) {
-      console.log('Authenticated');
-    } else {
-      console.log('Login Page');
-    }
-
-
     return (
-      <BrowserRouter>
-        <Switch>
-          <div>
-            <Login auth={auth}/>
-            <Search />
-          </div>
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <Search />
+      </div>
     );
   }
 }
 export default App;
-
-
-      // <BrowserRouter>
-      //   <Route path="/" component={Login} ></Route>
-      //   <Route path="/login" render={(props) => { Login }}></Route>
-
-      //   <div>
-      //     <Login auth={auth}/>
-      //     <Search />
-      //   </div>
-      // </BrowserRouter>
