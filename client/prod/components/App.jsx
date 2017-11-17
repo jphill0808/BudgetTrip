edb88530c1472171f0d89eaf4626afbede6ba321
Jacob <jphill0808@gmail.com>
2017-11-17 13:23:57 -0500

    progress

--- client/prod/components/App.jsx
+++ client/prod/components/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './login.jsx';
+import Auth from '../../../Auth/Auth.js';
 import Search from './Search.jsx';
+
+const auth = new Auth();
 
 class App extends React.Component {
   constructor(props) {
