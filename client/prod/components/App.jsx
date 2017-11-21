623e24a3df97f367152e3daca1f5887c46df4296
Jacob <jphill0808@gmail.com>
2017-11-21 12:24:38 -0500

    commit of the day

--- client/prod/components/App.jsx
+++ client/prod/components/App.jsx
@@ -1,34 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login.jsx';
-import Auth from '../../../Auth/Auth.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Router, Route, BrowserRouter, Redirect, withRouter } from 'react-router-dom';
 import Search from './Search.jsx';
-
-
-
+import Auth from '../../../Auth/Auth.js';
 const auth = new Auth();
 
+auth.handleAuthentication();
 
-// const auth = new Auth();
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
   }
 
-  // render() {
-  //   return (
-  //     <div>
-  //       <Login auth={auth}/>
-  //       <Search />
-  //     </div>
-  //   );
-  // }
   render() {
+    if (auth.isAuthenticated()) {
+      console.log('Authenticated');
+    } else {
+      console.log('Login Page');
+    }
+
+
     return (
-      <div>
-        <Search />
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Login auth={auth} />
+          <Search />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 export default App;
+
+
+      // <BrowserRouter>
+      //   <Route path="/" component={Login} ></Route>
+      //   <Route path="/login" render={(props) => { Login }}></Route>
+
+      //   <div>
+      //     <Login auth={auth}/>
+      //     <Search />
+      //   </div>
+      // </BrowserRouter>
--- client/prod/components/Search.jsx
+++ client/prod/components/Search.jsx
@@ -1,68 +1,131 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Paper from 'material-ui/Paper';
+import PlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId } from 'react-places-autocomplete';
+import TextField from 'material-ui/TextField';
+
 const axios = require('axios');
+
+const style = {
+  paper: {
+    height: 500,
+    width: 500,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+  },
+  container: {
+    width: 100,
+  },
+};
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      budget: null,
-      location: null,
-      startDate: null,
-      endDate: null
+      budget: '',
+      location: '',
+      startDate: '',
+      endDate: '',
     };
-
+    this.onChange = (input) => {
+      this.setState({input})
+    }
     this.monitor = this.monitor.bind(this);
     this.search = this.search.bind(this);
+
   }
 
-  monitor(event) {
-    const target = event.target;
-    const name = target.name;
-
-    this.setState(() => {
+  monitor(event, newVal) {
+/*    const target = event.target.value;
+    console.log(target);
+    const name = event.target.name;
+    console.log('name: ', name);
+*/  this.setState(() => {
       return {
-        [name]: target.value
+        location: newVal,
       };
+    });
+    console.log(this.state.location);
+  }
+
+  search(event) {
+    event.preventDefault();
+    axios({
+      method: 'post',
+      url: 'localhost:1130/search',
+      data: {
+        budget: this.state.budget,
+        location: this.state.location,
+        startDate: this.state.startDate,
+        endDate: this.state.endDate,
+      },
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
     });
   }
 
-  // search(event) {
-  //   event.preventDefault();
-  //   axios({
-  //     method: 'post',
-  //     url: 'localhost:1130/search',
-  //     data: {
-  //       budget: this.state.budget,
-  //       location: this.state.location,
-  //       startDate: this.state.startDate,
-  //       endDate: this.state.endDate
-  //     }
-  //   })
-  //   .then((data) => {
-  //     console.log(data);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
-
-  }
 
   render() {
+    console.log('location: ', this.state.location);
+    var inputProps = {
+      value: this.state.location,
+      onChange: this.onChange,
+      type: 'search',
+      autoFocus: true
+    }
     return (
-      <div className="container">
-        <form onSubmit={this.search} autoComplete='off'>
-          <label htmlFor="budget">Budget</label>
-          <input type="text" name="budget" onChange={this.monitor} value={this.state.budget}/>
-          <label htmlFor="location">Location</label>
-          <input type="text" align="right" name="location" onChange={this.monitor} value={this.state.location}/>
-          <label htmlFor=""></label>
-          <input type="date" name="start-date" onChange={this.monitor} value={this.state.startDate}/>
-          <label htmlFor=""></label>
-          <input type="date" name="end-date" onChange={this.monitor} value={this.state.endDate}/>
-          <button type="submit" value="Submit"> Submit </button>
-        </form>
+      <div className="container" style={style.container}>
+        <Paper style={style.paper} zDepth={2}>
+          <h2>Where do you want to go today?</h2>
+          <form onSubmit={this.search} autoComplete="off">
+            <label htmlFor="budget">Budget</label>
+            <input
+              type="text"
+              name="budget"
+              onChange={this.monitor}
+              value={this.state.budget}
+            />
+            <label htmlFor="location">Location</label>
+            <TextField
+              id="nameField"
+              name="location"
+              floatingLabelText="Name"
+              value={this.state.location}
+              onChange={(e, newVal) => {this.monitor(e, newVal)}}
+            />
+            <PlacesAutocomplete inputProps={inputProps} />
+            <input
+              type="text"
+              name="location"
+              id="location"
+              onChange={this.monitor}
+              value={this.state.location}
+            />
+            <label htmlFor="start-date"></label>
+            <input
+              type="date"
+              name="startDate"
+              onChange={this.monitor}
+              value={this.state.startDate}
+            />
+            <label htmlFor="end-date"></label>
+            <input
+              type="date"
+              name="endDate"
+              onChange={this.monitor}
+              value={this.state.endDate}
+            />
+            <button type="submit" value="Submit">
+              Submit
+            </button>
+          </form>
+        </Paper>
       </div>
     );
   }
 }
+
