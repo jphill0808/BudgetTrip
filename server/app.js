9c455741e6178b653b7bef42182da756de2aac23
elgeesound <elgeesound@gmail.com>
2017-11-20 18:16:04 -0800

    Expanded express routes with food(zomato), events(yelp)

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 var travel = require('./routes/travel.js').travel;
+var food = require('./routes/food.js').food;
+var events = require('./routes/events.js').events;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
 app.use('/api/travel', travel);
+app.use('/api/food', food);
+app.use('/api/events', events);
+
 
 app.get('/', (req, res) => {
   res.json('hello world');
---
+++ server/routes/events.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const axios = require('axios');
+const yelpAuthKEY = require('../../config.js')._yelpAuthKEY;
+const clientAuthID = require('../../config.js')._yelpAuthClientId;
+const yelpKEY = require('../../config.js')._yelpEventKey;
+
+const config = {
+  headers: {
+    'Authorization': `${yelpKEY}`,
+  }
+};
+
+router.get('/', (req, res, next) => {
+  axios(`https://api.yelp.com/v3/events?limit=25&radius=25&cll=37.7749,122.4194`, config)
+    .then((found) => {
+      console.log(found.data.events, 'found');
+      res.json(found.data.events);
+    }).catch((err) => {
+      console.log(err);
+    })
+});
+
+router.post('/', (req, res, next) => {
+  //test for new york city
+
+  // res.json('INSIDE FOOD!!');
+});
+
+
+
+module.exports.events = router;
+
+
+
