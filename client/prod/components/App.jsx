b5d6c6c0e57c6bd3c3c89017c89ed89d3302a9eb
Jacob <jphill0808@gmail.com>
2017-11-21 12:24:38 -0500

    commit of the day

--- client/prod/components/App.jsx
+++ client/prod/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login.jsx';
-import Auth from '../../../Auth/Auth.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Router, Route, BrowserRouter, Redirect, withRouter } from 'react-router-dom';
 import Search from './Search.jsx';
+import Auth from '../../../Auth/Auth.js';
 const auth = new Auth();
 
+auth.handleAuthentication();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
   }
 
   // render() {
@@ -21,11 +27,32 @@ class App extends React.Component {
   // }
 
   render() {
+    if (auth.isAuthenticated()) {
+      console.log('Authenticated');
+    } else {
+      console.log('Login Page');
+    }
+
+
     return (
-      <div>
-        <Search />
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Login auth={auth} />
+          <Search />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 export default App;
+
+
+      // <BrowserRouter>
+      //   <Route path="/" component={Login} ></Route>
+      //   <Route path="/login" render={(props) => { Login }}></Route>
+
+      //   <div>
+      //     <Login auth={auth}/>
+      //     <Search />
+      //   </div>
+      // </BrowserRouter>
