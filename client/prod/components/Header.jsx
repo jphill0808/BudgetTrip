94004d4e2ae56357588e610a3835531d9252301e
jphill0808 <jphill0808@gmail.com>
2018-06-22 18:10:01 -0400

    reset Header to new format to take router lists

--- client/prod/components/Header.jsx
+++ client/prod/components/Header.jsx
@@ -1,79 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Popover from 'material-ui/Popover';
-import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
-import DropDownMenu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-import Profile from './Profile.jsx';
+import { Link } from 'react-router-dom';
 
-const style = {
-  display: 'flex',
-  margin: '16, 32, 16, 0',
-  style: {
-    color: 'white',
-    background: 'green',
-  },
-};
 
-class Header extends React.Component {
+export default class Header extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      open: false,
-      displayProfile: false,
-    };
-    this.handleLogout = this.handleLogout.bind(this);
+    }
+    this.constructLinks = this.constructLinks.bind(this);
+    this.getMenu = this.getMenu.bind(this);
+  }
+  componentDidMount() {
+    this.constructLinks();
   }
 
-  handleTouchTap = event => {
-    event.preventDefault();
+  getMenu() {
+    let lists = {
+      'Search': '/Search',
+      'Budget': '/Budget'
+    };
+    return lists;
+  }
 
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleLogout = () => {
-    this.props.auth.logout();
-    window.location.reload();
-  };
+  constructLinks() {
+    // const menus = this.props.menus;
+    const menus = this.getMenu();
+    /*
+      {
+        linkname: '/linkname'...
+      }
+    */
+    let arr = Object.keys(menus);
+    // console.log('menus: ', menus);
+    // console.log('arr: ', arr);
+    return(
+      arr.map((link) => {
+        return (
+          <Link to={menus[link]}>{link}</Link>
+        )
+      })
+    )
+  }
 
   render() {
-    return (
-      <div className="header_container">
-        <a class="header_logo--button" href="/" />
-        <RaisedButton className="nav_menu" onClick={this.handleTouchTap} label="Account Info" />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-          onRequestClose={this.handleRequestClose}>
-          <Paper style={style}>
-            <DropDownMenu>
-              <MenuItem primaryText="Trip History" />
-              <MenuItem primaryText="User Profile" />
-              <MenuItem
-                onClick={e => {
-                  this.handleLogout(e);
-                }}
-                primaryText="Logout"
-              />
-            </DropDownMenu>
-          </Paper>
-        </Popover>
+    const links = this.constructLinks();
+    return(
+      <div>
+        {
+          links.map((eachLink) => {
+            return eachLink;
+          })
+        }
       </div>
-    );
+    )
   }
 }
-
-export default Header;
